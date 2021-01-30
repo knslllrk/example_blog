cf5cdcfd2151193fabc30a8420fd19dd12676f56
krllsnk <knslllrk@gmail.com>
2021-01-30 19:50:33 +0300

    .

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'django_db',
         'USER' : 'user_name',
-        'PASSWORD' : '1234',
+        'PASSWORD' : '',
         'HOST' : '127.0.0.1',
         'PORT' : '5432',
     }
